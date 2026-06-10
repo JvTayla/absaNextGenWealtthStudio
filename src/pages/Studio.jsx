3219db2eafdf423e2c8febb6f4e3605d287e0e69
JvTayla <vanaswegen.jordy@gmail.com>
2026-06-10 16:02:43 +0200

    Fixed images

--- src/pages/Studio.jsx
+++ src/pages/Studio.jsx
@@ -19,7 +19,7 @@ const STUDIOS = [
   {
     id: "car-comparison",
     path: "/studio/car-comparison",
-    icon: <img src="../public/used-luxury.jpg" alt="Used vs Luxury Car" />,
+    icon: <img src="../used-luxury.jpg" alt="Used vs Luxury Car" />,
     title: "Luxury Car vs Used Car",
     subtitle:
       "Finance a R600k luxury car or buy used and invest the difference?",
@@ -34,7 +34,7 @@ const STUDIOS = [
   {
     id: "rent-vs-buy",
     path: "/studio/rent-vs-buy",
-    icon: <img src="../public/rent-buy.png" alt="Rent vs Buy" />,
+    icon: <img src="../rent-buy.png" alt="Rent vs Buy" />,
     title: "Rent vs Buy in South Africa",
     subtitle:
       "Is owning a home actually better than renting in Johannesburg or Cape Town?",
@@ -50,10 +50,7 @@ const STUDIOS = [
     id: "offshore-allocation",
     path: "/studio/offshore-allocation",
     icon: (
-      <img
-        src="../public/local-offshore.png"
-        alt="Local vs Offshore Allocation"
-      />
+      <img src="../local-offshore.png" alt="Local vs Offshore Allocation" />
     ),
     title: "Local vs Offshore Allocation",
     subtitle:
